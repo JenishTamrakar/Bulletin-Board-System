7e9eb7a0ed8740905be277cff2108945dcc67d0a
Roshan <shrestharoshan776@gmail.com>
2019-11-25 08:58:28 +0545

    folders created

--- src/BBS/Main.java
+++ src/BBS/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package BBS;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("loginScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/loginScreen.fxml"));
         primaryStage.setTitle("Bulletin Board System");
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
--- src/BBS/controllers/registerScreenController.java
+++ src/BBS/controllers/registerScreenController.java
@@ -1,4 +1,4 @@
-package sample;
+package BBS.controllers;
 
 public class registerScreenController {
 }
